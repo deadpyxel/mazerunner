2756e84ae90cd594e5308882b5f99dceca559ae0
Robson Cruz <ghost0092@gmail.com>
2024-02-23 19:48:06 -0300

    chore(main.py): update main to test new methods
    Also removed line drawing tests as this is now being done during cell drawing

--- mazerunner/main.py
+++ mazerunner/main.py
@@ -6,22 +6,15 @@ from mazerunner.maze import Cell
 
 def main() -> int:
     win = Window(1280, 800)
-    lines = []
-    for _ in range(16):
-        p1 = Point(x=random.randint(0, 400), y=random.randint(0, 300))
-        p2 = Point(x=random.randint(400, 800), y=random.randint(300, 600))
-        lines.append(Line(start=p1, end=p2))
 
-    for line in lines:
-        win.draw_line(
-            line,
-            fill_colour=random.choice(
-                ["red", "green", "blue", "yellow", "cyan", "magenta", "black", "white"]
-            ),
-        )
-
-    c = Cell(Point(10, 10), Point(150, 150), win)
-    c.draw()
+    c1 = Cell(Point(10, 10), Point(110, 110), win)
+    c1.draw()
+    c2 = Cell(Point(110, 10), Point(210, 110), win)
+    c2.draw()
+    c3 = Cell(Point(10, 110), Point(110, 210), win)
+    c3.draw()
+    c1.draw_move(c2)
+    c1.draw_move(c3, undo=True)
 
     win.wait_for_close()
 
