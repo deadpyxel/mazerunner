ed6c2febd091b4b3a0bc4e8cd97260defa931385
Robson Cruz <ghost0092@gmail.com>
2024-02-24 15:52:56 -0300

    chore(main.py): update entrypoint

--- mazerunner/main.py
+++ mazerunner/main.py
@@ -6,6 +6,7 @@ def main() -> int:
     win = Window(1280, 800)
 
     maze = Maze(x1=10, y1=10, num_rows=10, num_cols=15, cell_size=20, win=win)
+    print(f"Maze created: {maze}")
 
     win.wait_for_close()
 
