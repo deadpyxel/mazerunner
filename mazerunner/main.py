3bb7c6a4281354bcf04e058b68aac8f64cc7eb9e
Robson Cruz <ghost0092@gmail.com>
2024-02-24 15:26:00 -0300

    chore(main.py): update entrypoint

--- mazerunner/main.py
+++ mazerunner/main.py
@@ -1,20 +1,11 @@
-import random
-
-from mazerunner.core import Line, Point, Window
-from mazerunner.maze import Cell
+from mazerunner.core import Window
+from mazerunner.maze import Maze
 
 
 def main() -> int:
     win = Window(1280, 800)
 
-    c1 = Cell(Point(10, 10), Point(110, 110), win)
-    c1.draw()
-    c2 = Cell(Point(110, 10), Point(210, 110), win)
-    c2.draw()
-    c3 = Cell(Point(10, 110), Point(110, 210), win)
-    c3.draw()
-    c1.draw_move(c2)
-    c1.draw_move(c3, undo=True)
+    maze = Maze(x1=10, y1=10, num_rows=10, num_cols=15, cell_size=20, win=win)
 
     win.wait_for_close()
 
